3fee7b1a88c10d1de9cf5ef75a3befcbf3eb252c
Napong Jantaranimi <Berkito_99@hotmail.com>
2018-09-26 00:51:09 +0700

    add item of group setting

--- src/app/setting/setting-item/setting-item.component.ts
+++ src/app/setting/setting-item/setting-item.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { map } from 'rxjs/operators';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { map, timestamp } from 'rxjs/operators';
 
 @Component({
   selector: 'app-setting-item',
@@ -14,61 +14,55 @@ export class SettingItemComponent implements OnInit {
   private groupId: any;
   private itemAction: string = 'add';
   private itemUpdateId: string = '';
-  private items: { name: string }[] = [];
-  private itemList;
+  private items: any;
   private itemCollection: AngularFirestoreCollection;
+
   private itemForm = this.fb.group({
-    name: [''],
-    // items: this.fb.array([
-    //   this.fb.control('')
-    // ])
+    name: ['']
   });
 
   constructor(private route: ActivatedRoute, private router: Router, private fb: FormBuilder, private db: AngularFirestore) {
     this.groupId = this.route.snapshot.paramMap.get('id');
-    this.itemCollection = this.db.collection('group-item/' + this.groupId);
-    this.itemList = this.itemCollection.snapshotChanges().pipe(map(action => {
-      return action.map(a => {
-        const id = a.payload.doc.id;
-        const data = a.payload.doc.data();
-        return {
-          id,
-          ...data
-        }
+    this.itemCollection = this.db.collection('item', ref => ref.where('groupId', '==', this.groupId).orderBy('createDate'));
+    this.items = this.itemCollection.snapshotChanges().pipe(map(action => {
+      return action.map(x => {
+        const id = x.payload.doc.id;
+        const data = x.payload.doc.data()
+        return { id, ...data };
       })
     }));
+
   }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    // let newName = this.itemForm.value.name;
-    // if (this.itemAction === 'add') {
-    //   this.items.push({ name: newName });
-    //   console.log('add item: ', this.itemForm.value.name);
-    //   this.db.list('group-item/' + this.groupId).set('items', this.items);
-    // } else {
-    //   this.items[this.itemUpdateId] = { name: this.itemForm.value.name };
-    //   console.log('update item: ', this.itemUpdateId);
-    //   this.db.list('group-item/' + this.groupId).set('items', this.items);
-    //   this.setDefaultAction();
-    // }
-    // this.itemForm.reset();
+    let newName = this.itemForm.value.name;
+    if (this.itemAction === 'add') {
+      console.log('add item: ', this.itemForm.value.name);
+      let timeStamp = new Date();
+      this.itemCollection.add({ name: newName, groupId: this.groupId, createDate: timeStamp });
+    } else {
+      this.items[this.itemUpdateId] = { name: this.itemForm.value.name };
+      console.log('update item: ', this.itemUpdateId);
+      this.itemCollection.doc(this.itemUpdateId).update({ name: newName })
+      this.setDefaultAction();
+    }
+    this.itemForm.reset();
   }
 
-  onEdit(i) {
-    console.log('on edit: ', i)
+  onEdit(item) {
+    console.log('on edit: ', item.id)
     this.itemAction = 'edit';
-    this.itemUpdateId = i;
-    this.itemForm.controls['name'].setValue(this.items[i].name);
+    this.itemUpdateId = item.id;
+    this.itemForm.controls['name'].setValue((item.name));
   }
 
-  onDelete(i) {
-    // console.log('remove key: ', i);
-    // this.items.splice(i, 1);
-    // this.db.list('group-item/' + this.groupId).set('items', this.items);
-    // this.setDefaultAction();
+  onDelete(item) {
+    console.log('remove key: ', item);
+    this.itemCollection.doc(item.id).delete();
+    this.setDefaultAction();
   }
 
   onCancel() {
